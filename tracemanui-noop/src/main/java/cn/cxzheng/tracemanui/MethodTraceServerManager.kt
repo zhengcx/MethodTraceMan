c5fc3ffc28446d39599952b8f40a9270b04eaec5
cxzheng <cxzheng@ctrip.com>
2019-10-27 11:51:51 +0800

    增加noop库用于线上包集成

---
+++ tracemanui-noop/src/main/java/cn/cxzheng/tracemanui/MethodTraceServerManager.kt
@@ -0,0 +1,19 @@
+package cn.cxzheng.tracemanui
+
+import android.content.Context
+
+/**
+ * Create by cxzheng on 2019-10-25
+ */
+object MethodTraceServerManager {
+
+    @Synchronized
+    @JvmOverloads
+    fun startService(context: Context, port: Int = 0) {
+    }
+
+    @Synchronized
+    fun stopService() {
+
+    }
+}
---
+++ tracemanui-noop/src/main/java/cn/cxzheng/tracemanui/TraceMan.java
@@ -0,0 +1,14 @@
+package cn.cxzheng.tracemanui;
+
+/**
+ * Create by cxzheng on 2019-10-25
+ */
+public class TraceMan {
+
+
+    public static void start(String name) {
+    }
+
+    public static void end(String name) {
+    }
+}
